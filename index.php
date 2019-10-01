bc24125529cbb13b64ecf0ea3553646e380432f5
saulApp <saul.aranda0214@gmail.com>
2019-10-01 01:36:28 -0500

    mi primer comit

---
+++ index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="es">
+<head >
+	<title>PAGINA WEB-PROYECTO</title>
+	<meta charset="utf-8">
+	<!--
+	Links Externos
+	-->
+	<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+	<meta name="viewport" content="width=device-width,user-scalable=no">
+</head>
+<body>
+
+	<a id="whatsapp" href="https://api.whatsapp.com/send?phone=993009395"><img src="imagenes/whatsapp.png"></a>
+	<header>
+		<div class="menu">
+			<div class="contenedor">
+				<p class="logo">SAUL ARANDA </p>
+				<img class="menu_icon" src="imagenes/menu.png">
+				<nav>
+					<ul class="lista_menu">
+						<li><a href="#sobre_mi">Sobre mi</a></li>
+						<li><a href="#Servicios">Servicios</a></li>
+						<li><a href="#mis_trabajos">mis trabajos</a></li>
+						<li><a href="#porque_trabajar_conmigo">¿Por que trabajar conmigo?</a></li>
+						<li><a href="#blog">Blog</a></li>
+						<li><a href="#contacto">Contacto</a></li>
+					</ul>
+				</nav>				
+			</div>
+		</div>
+
+		<div class="" id="contenedor-titulo-flex">
+			<div class="contenedor_titulo">
+			<h1>SAUL ARANDA CHAVEZ</h1>
+			<h2>Diseñador & Programador Web</h2>
+			<a href="">VER MIS TRABAJOS</a>
+			</div>
+		</div>
+		</header>
+
+		<!--MAIN  -->
+		<main>
+			<section id="sobre_mi">
+				<div class="contenedor">
+					<h3>Sobre mi</h3>
+					<div class="contenedor_sobre_mi">
+						<div class="computadoras">
+							<img src="imagenes/computadora.png">
+						</div>
+						<div class="texto">
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+							<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+							<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+							<a href="">Ver mis trabajos</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="Servicios">
+				<div class="contenedor">
+					<h3>Servicios</h3>
+					<div class="contenedor_servicios">
+						<div class="servicio violeta">
+						<h4>Diseño web</h4>
+						<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+						<img class="icono" src="imagenes/monitor.png">
+						<img class="ondas" src="imagenes/waves.png">
+						</div>
+
+						<div class="servicio celeste">
+						<h4>Diseño web</h4>
+						<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+						<img class="icono" src="imagenes/monitor.png">
+						<img class="ondas" src="imagenes/waves.png">
+					</div>
+
+
+					<div class="servicio violeta">
+						<h4>Diseño web</h4>
+						<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+						<img class="icono" src="imagenes/monitor.png">
+						<img class="ondas" src="imagenes/waves.png">
+					</div>
+
+					<div class="servicio celeste">
+						<h4>Diseño web</h4>
+						<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+						<img class="icono" src="imagenes/monitor.png">
+						<img class="ondas" src="imagenes/waves.png">
+					</div>
+
+					<div class="servicio violeta">
+						<h4>Diseño web</h4>
+						<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+						<img class="icono" src="imagenes/monitor.png">
+						<img class="ondas" src="imagenes/waves.png">
+					</div>
+
+					<div class="servicio celeste">
+						<h4>Diseño web</h4>
+						<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+						<img class="icono" src="imagenes/monitor.png">
+						<img class="ondas" src="imagenes/waves.png">
+						</div>
+					</div>
+
+				</div>
+			</section>
+
+			<section id="mis_trabajos">
+				<div class="contenedor">
+					<h3>Mis Trabajos</h3>
+					<div class="owl-carousel owl-theme">
+				    <div class="item">
+				    	<a href="https://www.google.com/">
+				    	<img src="imagenes/sassy.jpg"></a>
+				    	<p>Proyecto Web</p>
+				    </div>
+				    <div class="item"><img src="imagenes/chaia.jpg">
+				    <a href="www.google.com.pe"></a>
+				    	<p>Proyecto Web</p></div>
+				    <div class="item"><img src="imagenes/alimentar.jpg">
+				    <a href="www.google.com.pe"></a>
+				    	<p>Proyecto Web</p></div>
+				    <div class="item"><img src="imagenes/isip.jpg">
+				    <a href="www.google.com.pe"></a>
+				    	<p>Proyecto Web</p></div>
+				    <div class="item"><img src="imagenes/chaia.jpg">
+				    <a href="www.google.com.pe"></a>
+				    	<p>Proyecto Web</p></div>
+				    <div class="item"><img src="imagenes/alimentar.jpg">
+				    <a href="www.google.com.pe"></a>
+				    	<p>Proyecto Web</p></div>
+				</div>
+				</div>
+			</section>
+
+			<section id="porque_trabajar_conmigo">
+				<div class="contenedor">
+					<h3>¿Por que trabajar Conmigo?</h3>
+					<div class="contenedor_trabajar">
+						<div class="item">
+					<ul >
+						<li><img src="imagenes/velocidad.png"></li>
+						<li>Velocidad</li>
+						<li>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</li>
+					</ul>
+					</div>
+
+								<div class="item">
+					<ul >
+						<li><img src="imagenes/mantenimiento.png"></li>
+						<li>Velocidad</li>
+						<li>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</li>
+					</ul>
+					</div>
+								<div class="item">
+					<ul >
+						<li><img src="imagenes/mobile2.png"></li>
+						<li>Velocidad</li>
+						<li>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</li>
+					</ul>
+					</div>
+
+					</div>
+				</div>
+			</section>
+
+			<section id="blog">
+				<div class="contenedor">
+					<h3>BLOG</h3>
+					<div class="contenedor_publicaciones">
+						<div class="publicacion">
+							<img src="imagenes/blog1.jpg">
+							<div class="contenido">
+							<h4>No alcanza solamente una pagia</h4>
+							<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+							<a href="">Leer más →</a>
+							</div>
+						</div>
+
+						<div class="publicacion">
+							<img src="imagenes/blog2.jpg">
+							<div class="contenido">
+							<h4>No alcanza solamente una pagia</h4>
+							<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+							<a href="">Leer más →</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="que_esperas">
+				<div class="contenedor">
+					<h3>¿Que esperas?</h3>
+					<a href="">Contactanos</a>
+				</div>
+			</section>
+
+			<section id="contacto">
+				<?php
+				if(@$_GET['i']=='ok'){
+					?>
+					<h3>La consulta se envio correctamente.
+					Nos comunicaremos a la brevedad.</h3>
+
+					<?php
+				}else{?>
+
+
+				<div class="contenedor">
+					<h3>CONTACTO</h3>
+					<form action="envio-formulario.php" method="POST" enctype="multipart/form-data">
+						<input type="text" placeholder="Nombre" name="nombre" required="">
+						<input type="email" placeholder="Email" name="email" required="">
+						<textarea placeholder="Mensaje" name="mensaje" required=""></textarea>
+						<input type="submit" value="ENVIAR MENSAJE" name="" required="">
+					</form>
+				<?php } ?>
+
+				<div class="contacto_infor">
+					<div class="mail"> <img src="imagenes/mail-icon.png"> saul.aranda@live.com</div>
+					<div class="whatsapp"><img src="imagenes/whatsapp.png">993009395</div>
+				</div>
+				</div>
+			</section>
+		</main>
+
+		<footer>
+			<div class="contenedor">
+				<div class="redes">
+					<a href="#"><img src="imagenes/facebook.png" alt="imagen de facebook"></a>
+					<a href="#"><img src="imagenes/linkedin.png" alt="imagen de linkedin"></a>
+					<a href="#"><img src="imagenes/github.png" alt="imagen de github"></a>
+					<a href="#"><img src="imagenes/medium.png" alt="imagen de medium"></a>
+				</div>
+				<div class="parrafo">
+					<p>
+						Derehos reservados 
+					</p>
+				</div>
+			</div>
+		</footer>
+
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="owl/owl.carousel.min.js"></script>
+<script type="text/javascript">
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+</script>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(window).scroll(function(){
+			  scroll =$(window).scrollTop();
+			 	if(scroll>100){
+			 		$(".menu").css({"position":"fixed"});
+			 		$(".menu").css({"width":"100%"});
+			 		$(".menu").css({"top":"0"});
+			 		$(".menu").css({"background":"#fff"});
+			 		$(".menu a").css({"color":"#000"});
+			 		$(".logo").css({"color":"#000"});
+			 		$(".menu").css({"box-shadow":"rgba(0, 0,0,0.22) 6px 1px 1px"});
+			 		$(".menu").css({"z-index":"100"});
+			 	}else{
+			 			// cuando no hay scroll
+			 		$(".menu").css({"position":"relative"});	
+			 		$(".menu").css({"background":"transparent"});
+			 		$(".menu").css({"box-shadow":"0 0 0"});
+			 		$(".menu a").css({"color":"#fff"});
+			 		$(".logo").css({"color":"#fff"});
+			 	}
+		});
+		$(".menu_icon").click(function(){
+				$("header nav").slideToggle();
+		});
+
+
+
+	});
+
+</script>
+</body>
+</html>
